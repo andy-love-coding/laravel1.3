d3b464f590b6c33d5339c79489547f06ba6065e7
andy <andy.liujin@foxmail.com>
2020-01-06 12:21:56 +0000

    11.2粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,42 @@ class User extends Authenticatable
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
     }
+
+    // 本 model 为博主 （博主的粉丝类别）
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 本 model 为粉丝 （粉丝的博主列表）
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取关
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    // 是否关注
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
+    }
 }
