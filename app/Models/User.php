9a70fd053ce6e8517ef0c844644e1e5192600bda
andy <andy.liujin@foxmail.com>
2020-01-06 07:44:18 +0000

    10.2微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,20 +11,10 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -47,5 +37,10 @@ class User extends Authenticatable
     {
         $hash = md5(strtolower(trim($this->attribute['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
     }
 }
---
+++ database/migrations/2020_01_06_073401_create_statuses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('content');
+            $table->integer('user_id')->index(); // 按 user_id 搜微博，所以加索引
+            $table->index(['created_at']); // 按创建时间排序，所以加索引
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
