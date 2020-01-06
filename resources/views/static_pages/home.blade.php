415dda53d10e3eebec767e4cc717ed6544a9cd50
andy <andy.liujin@foxmail.com>
2020-01-06 10:44:39 +0000

    10.5首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,6 +7,8 @@
         <section class="status_form">
           @include('shared._status_form')
         </section>
+        <h4>微博列表</h4>
+        @include('shared._feed')
       </div>
 
       <div class="col-md-4">
